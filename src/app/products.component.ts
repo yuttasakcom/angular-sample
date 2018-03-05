8f49647418cbd96fde3635da823e49031663520d
YoProgrammer <yuttasakcom@gmail.com>
2018-03-05 17:29:39 +0700

    create products and product component

---
+++ src/app/products.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html'
+})
+export class ProductsComponent {
+  productName = 'A Book'
+  isDisabled = true
+  products = ['A Book', 'A Tree']
+
+  constructor() {
+    setTimeout(() => {
+      this.isDisabled = false
+    }, 3000)
+  }
+
+  onAddProduct() {
+    this.products.push(this.productName)
+  }
+}
